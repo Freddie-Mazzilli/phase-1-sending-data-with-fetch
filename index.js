10f9ae11be68152c5310f4f9cc9e52bafc28edbe
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-04-14 15:33:14 -0400

    Completes sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+
+function submitData(name, email){ 
+    const newUser = {
+        name: name,
+        email: email
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+    .then(res => res.json())
+    .then(newUser => {
+        document.body.append(newUser.id)
+    })
+    .catch(error => {
+        document.body.append(error)
+    })
+}
+
+submitData("joe", "joe@joe.com")
+
+
+// fetch("http://localhost:3000/foods", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(newFood)
+//     })
+// })
